1336de00fa7af23e285454a6dccdd1f5253bb385
elninho22 <andrejulianom@gmail.com>
2019-04-22 14:17:02 -0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -46,7 +46,7 @@ $this->title = 'SigFolha - Studiorama ';
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="exampleModalLabel">Conxfirma reprovar folha ?</h4>
+                <h4 class="modal-title" id="exampleModalLabel">Confirma reprovar folha ?</h4>
               </div>
               <div class="modal-body">
                 <form>
